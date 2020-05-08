07854d1cb7732d1367db728b7f9be5ebaf1745f1
ian13456 <21ianh1@students.tas.tw>
2020-05-08 12:18:19 +0800

    feat: moving stars

--- src/js/game/stars.js
+++ src/js/game/stars.js
@@ -5,12 +5,14 @@ class Stars {
     this.render = render
 
     this.stars = []
+
+    this.initStars()
   }
 
   initStars = () => {
     for (let i = 0; i < this.count; i++) {
       const x = Math.random() * CANVAS_WIDTH
-      const y = Math.random() * CANVAS_HEIGHT
+      const y = Math.random() * (CANVAS_HEIGHT - this.offset)
 
       // position and color
       this.stars.push([{ x, y }, Math.random() / 2])
@@ -29,6 +31,12 @@ class Stars {
   }
 
   draw = () => {
+    this.stars.forEach(([pos]) => {
+      pos.x += STARS_SPEED
+      if (pos.x > CANVAS_WIDTH + STAR_RADIUS / 2) {
+        pos.x = -STAR_RADIUS / 2
+      }
+    })
     this.stars.forEach(this.drawStar)
   }
 }
