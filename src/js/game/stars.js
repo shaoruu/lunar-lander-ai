20e512bffa59488d6f2a31d61900da1057c36ad8
ian13456 <21ianh1@students.tas.tw>
2020-05-09 01:21:21 +0800

    fix: reduce loops

--- src/js/game/stars.js
+++ src/js/game/stars.js
@@ -33,13 +33,16 @@ class Stars {
   }
 
   draw = () => {
-    this.stars.forEach(([pos]) => {
+    this.stars.forEach((star) => {
+      const [pos] = star
+
       pos.x += STARS_SPEED
       if (pos.x > CANVAS_WIDTH + STAR_RADIUS / 2) {
         pos.x = -STAR_RADIUS / 2
         pos.y = Math.random() * (CANVAS_HEIGHT - this.offset)
       }
+
+      this.drawStar(star)
     })
-    this.stars.forEach(this.drawStar)
   }
 }
